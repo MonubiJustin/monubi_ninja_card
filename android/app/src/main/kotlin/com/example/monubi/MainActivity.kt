990fcea2f9e88e388eab450357ff078af17a8a94
MonubiJustin <monubijustin@gmail.com>
2024-10-11 11:15:38 +0300

    updated original app with latest changes

---
+++ android/app/src/main/kotlin/com/example/monubi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.monubi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: monubi(),
+  ));
+}
+
+class monubi extends StatefulWidget {
+  const monubi({super.key});
+
+  @override
+  State<monubi> createState() => _monubiState();
+}
+
+class _monubiState extends State<monubi> {
+
+  int ninjaLevel = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text(
+          'Monubi ID Card',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        backgroundColor: Colors.grey[850],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+
+            setState(() {
+              ninjaLevel += 1;
+            });
+          },
+      backgroundColor: Colors.grey[800],
+      child: Icon(Icons.add, color: Colors.white)),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/two.jpg'), radius: 40.0),
+            ),
+            Divider(color: Colors.grey[400], height: 60, thickness: 2),
+            Text('NAME',
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
+            SizedBox(height: 10),
+            Text('Monubi-Justin',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    letterSpacing: 2.0)),
+            SizedBox(height: 30),
+            Text('Current Ninja Level',
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
+            SizedBox(height: 10),
+            Text('$ninjaLevel',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0)),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Icon(Icons.email, color: Colors.grey[400]),
+                SizedBox(width: 10),
+                Text('monubijustin@gmail.com',
+                    style: TextStyle(
+                        color: Colors.grey[400],
+                        letterSpacing: 1,
+                        fontSize: 18))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
